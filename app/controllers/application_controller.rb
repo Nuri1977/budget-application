829fe58d24e47ac0330d9da02119323b6b480730
Nuri1977 <nurilacka@gmail.com>
2022-05-20 14:14:39 +0200

    Add name field to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:email, :password, :password_confirmation)
+      u.permit(:email, :password, :password_confirmation, :name)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:email, :password, :password_confirmation, :current_password)
+      u.permit(:email, :password, :password_confirmation, :current_password, :name)
     end
   end
 
