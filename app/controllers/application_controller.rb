3c8026488e003c7f273d328c007eb87de7440492
Nuri1977 <nurilacka@gmail.com>
2022-05-23 12:39:31 +0200

    Style sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:email, :password, :password_confirmation, :name)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:email, :password, :password_confirmation, :current_password, :name)
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
     end
   end
 end
