c0814711b05fc79d096eebf8e4834e18af269734
Nuri1977 <nurilacka@gmail.com>
2022-05-24 15:10:32 +0200

    Add specs for models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Nuri', email: 'test@email', password: 'password')
+    @category = Category.new(user: @user, name: 'My title', icon: 'https://cdn-icons-png.flaticon.com/128/2731/2731636.png')
+  end
+
+  it 'if all atributes pass validation' do
+    expect(@category).to be_valid
+  end
+
+  it 'if there is title' do
+    @category.name = nil
+    expect(@category).not_to be_valid
+  end
+
+  it 'if there is max 250 characters' do
+    @category.name = 'Testing lenght'
+    expect(@category).to be_valid
+  end
+
+  it 'if icon url is invalid' do
+    @category.icon = nil
+    expect(@category).not_to be_valid
+  end
 end
